db808bbfc0a48c46141b345d4dc1c76c949cd180
Chinmay277 <chinmaybodake277@gmail.com>
2026-01-09 23:29:13 +0530

    dockerfile, jenkinsfile, yamls commit.

---
+++ cicd/src/main/java/com/chinmay/cicd/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.chinmay.cicd.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class Hello {
+
+    @GetMapping("hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello world");
+
+    }
+
+
+}
